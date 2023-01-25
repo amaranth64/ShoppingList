16568a47a592756f94bd83cf60faccd4033a0049
Vlad Borisov <amaranth64@yandex.ru>
2023-01-25 13:09:42 +0400

    ActivityResultLauncher для NoteFragment

--- app/src/main/java/ru/worklight64/shoppinglist/activities/NewNoteActivity.kt
+++ app/src/main/java/ru/worklight64/shoppinglist/activities/NewNoteActivity.kt
@@ -1,11 +1,13 @@
 package ru.worklight64.shoppinglist.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import ru.worklight64.shoppinglist.R
 import ru.worklight64.shoppinglist.databinding.ActivityNewNoteBinding
+import ru.worklight64.shoppinglist.fragments.NoteFragment
 
 class NewNoteActivity : AppCompatActivity() {
     private lateinit var form: ActivityNewNoteBinding
@@ -23,14 +25,23 @@ class NewNoteActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == R.id.id_save){
-
+            setMainResult()
         } else if (item.itemId == android.R.id.home){
             finish()
         }
         return super.onOptionsItemSelected(item)
     }
 
-    fun actionBarSettings(){
+    private fun setMainResult(){
+        val i = Intent().apply {
+            putExtra(NoteFragment.TITLE_KEY, form.edTitle.text.toString())
+            putExtra(NoteFragment.DESC_KEY, form.edDescription.text.toString())
+        }
+        setResult(RESULT_OK, i)
+        finish()
+    }
+
+    private fun actionBarSettings(){
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
--- app/src/main/java/ru/worklight64/shoppinglist/fragments/NoteFragment.kt
+++ app/src/main/java/ru/worklight64/shoppinglist/fragments/NoteFragment.kt
@@ -1,10 +1,15 @@
 package ru.worklight64.shoppinglist.fragments
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+
 import androidx.fragment.app.activityViewModels
 import ru.worklight64.shoppinglist.activities.MainApp
 import ru.worklight64.shoppinglist.activities.NewNoteActivity
@@ -14,16 +19,19 @@ import ru.worklight64.shoppinglist.db.MainViewModel
 class NoteFragment : BaseFragment() {
 
     private lateinit var fragForm: FragmentNoteBinding
+    private lateinit var editLauncher: ActivityResultLauncher<Intent>
+
     private val mainViewModel: MainViewModel by activityViewModels{
         MainViewModel.MainViewModelFactory((context?.applicationContext as MainApp).database)
     }
 
     override fun onClickNew() {
-        startActivity(Intent(activity, NewNoteActivity::class.java))
+        editLauncher.launch(Intent(activity, NewNoteActivity::class.java))
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        onEditResult()
     }
 
     override fun onCreateView(
@@ -35,7 +43,19 @@ class NoteFragment : BaseFragment() {
         return fragForm.root
     }
 
+    private fun onEditResult(){
+        editLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ){
+            if (it.resultCode == Activity.RESULT_OK){
+                Toast.makeText(activity, it.data?.getStringExtra(TITLE_KEY),Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
     companion object {
+        const val TITLE_KEY = "title_key"
+        const val DESC_KEY = "desc_key"
         @JvmStatic
         fun newInstance() = NoteFragment()
     }
