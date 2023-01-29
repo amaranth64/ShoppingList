ca4c9dead7bd5e8a0e7296e8f2da0ea6d8581858
Vlad Borisov <amaranth64@yandex.ru>
2023-01-29 17:51:26 +0400

    сокрытие элементов меню

--- app/src/main/java/ru/worklight64/shoppinglist/activities/ShopListActivity.kt
+++ app/src/main/java/ru/worklight64/shoppinglist/activities/ShopListActivity.kt
@@ -3,6 +3,7 @@ package ru.worklight64.shoppinglist.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.viewModels
 import androidx.fragment.app.activityViewModels
 import ru.worklight64.shoppinglist.R
@@ -14,6 +15,7 @@ import ru.worklight64.shoppinglist.entities.ShoppingListName
 class ShopListActivity : AppCompatActivity() {
     private lateinit var form: ActivityShopListBinding
     private var shopListName: ShoppingListName? = null
+    private lateinit var saveItemMenu: MenuItem
 
     private val mainViewModel: MainViewModel by viewModels{
         MainViewModel.MainViewModelFactory((applicationContext as MainApp).database)
@@ -27,7 +29,27 @@ class ShopListActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.shop_list_menu, menu)
+        saveItemMenu = menu?.findItem(R.id.save_item)!!
+        val newItem = menu.findItem(R.id.new_item)
+        newItem.setOnActionExpandListener(expandActionView())
+        saveItemMenu.isVisible = false
         return true
+    }
+
+    private fun expandActionView():MenuItem.OnActionExpandListener{
+        return object : MenuItem.OnActionExpandListener{
+            override fun onMenuItemActionExpand(p0: MenuItem): Boolean {
+                saveItemMenu.isVisible = true
+                return true
+            }
+
+            override fun onMenuItemActionCollapse(p0: MenuItem): Boolean {
+                saveItemMenu.isVisible = false
+                invalidateOptionsMenu()
+                return true
+            }
+
+        }
     }
 
     private fun init(){
